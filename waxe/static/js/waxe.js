3d71786d81a982856508009a69cdf283f920c6a8
Aurélien Matouillot <a.matouillot@gmail.com>
2013-05-20 22:48:56 +0200

    * Display the breadcrumb * Use pushState * Display messages to the user

--- waxe/static/js/waxe.js
+++ waxe/static/js/waxe.js
@@ -1,33 +1,67 @@
+$.fn.message.defaults.css.position = 'fixed';
+$.fn.message.defaults.css.top = '76px';
+
+
+
 (function($){
     "use strict";
 
     var waxe = {
-        content_events: function(){
-            $('section#section-content').each(function(){
-                $(this).find('#file-navigation a[data-href]').click(function(){
-                    waxe.update_page($(this).data('href'));
-                });
+        link_events: function(container){
+            container.find('a[data-href]').click(function(e){
+                e.preventDefault();
+                window.history.pushState(
+                    {'json_url': $(this).data('href')},
+                    $(this).text(),
+                    $(this).attr('href')
+                    );
+                waxe.update_page($(this).data('href'));
             });
         },
         update_page: function(url){
+            $(document).message('info', 'Loading...', {'autohide': false});
             $.ajax({
                 type: 'GET',
                 url: url,
                 dataType: 'json',
                 success: function(data, textStatus, jqXHR){
-                    $('section#section-content').html(data.content);
-                    waxe.content_events();
+                    var $section = $('section#section-content');
+                    $(document).scrollTop(0);
+                    if (data.error_msg){
+                        $(document).message('error', data.error_msg);
+                    }
+                    else if(data.content){
+                        var $content = $section.find('.content');
+                        $content.html(data.content);
+                        waxe.link_events($content);
+                        $(document).message('info', 'Loaded!');
+                    }
+                    var $breadcrumb = $section.find('.breadcrumb');
+                    if (data.breadcrumb){
+                        $breadcrumb.html(data.breadcrumb);
+                        waxe.link_events($breadcrumb);
+                    }
+                    else{
+                        $breadcrumb.html('');
+                    }
                 },
                 error: function(jqXHR, textStatus, errorThrown){
-                    alert('Error ajax loading');
+                    var msg = jqXHR.status + ' ' + jqXHR.statusText + ': ' + url;
+                    $(document).message('error', msg);
                 }
             });
         }
     };
 
+    window.onpopstate = function(e) {
+        if(e.state != null){
+            waxe.update_page(e.state.json_url);
+        }
+    };
 
     $(document).ready(function(){
-        waxe.content_events();
+        waxe.link_events($('section#section-content'));
     });
 
 })(jQuery);
+
