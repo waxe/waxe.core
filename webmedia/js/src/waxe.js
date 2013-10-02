3c71ac9aa51c29972271b38c250625cbafaa2cd2
Aurélien Matouillot <a.matouillot@gmail.com>
2013-10-03 00:41:04 +0200

    js file to set the plugins options

--- webmedia/js/src/waxe.js
+++ webmedia/js/src/waxe.js
@@ -1,20 +1,3 @@
-$.fn.message.defaults.css.position = 'fixed';
-$.fn.message.defaults.css['z-index'] = 2000;
-$.fn.message.defaults.template.message = '<div class="alert">\
-  <button type="button" class="close">&times;</button>\
-  <span class="text"/>\
-</div>';
-$.fn.message.defaults.extra_class = {
-    info: 'alert-info',
-    success: 'alert-success',
-    error: 'alert-error'
-};
-$.fn.message.defaults.selector.close_btn = '.close';
-
-$.fn.filebrowser.defaults.template.nav_file = '<li><i class="icon-file"></i></li>';
-$.fn.filebrowser.defaults.template.nav_folder = '<li><i class="icon-folder-close"></i></li>';
-
-
 (function($){
     "use strict";
 
@@ -470,4 +453,3 @@ $.fn.filebrowser.defaults.template.nav_folder = '<li><i class="icon-folder-close
     });
 
 })(jQuery);
-
