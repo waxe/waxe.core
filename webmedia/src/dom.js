9d4e607d52d21868e75eca9fbede0f662787957c
Aurélien Matouillot <a.matouillot@gmail.com>
2013-10-09 19:28:29 +0200

    Add tests for waxe.dom

--- webmedia/src/dom.js
+++ webmedia/src/dom.js
@@ -3,45 +3,51 @@ var waxe = waxe || {};
 (function($, ns){
     "use strict";
 
+    var onclick = function(e){
+        e.preventDefault();
+        window.history.pushState(
+            {'json_url': $(this).data('href')},
+            $(this).text(),
+            $(this).attr('href')
+            );
+        waxe.dom.load($(this).data('href'));
+    };
+
     ns.dom = {
         addPushStateOnLinks: function(container){
-            container.find('a[data-href]').click(function(e){
-                e.preventDefault();
-                window.history.pushState(
-                    {'json_url': $(this).data('href')},
-                    $(this).text(),
-                    $(this).attr('href')
-                    );
-                waxe.dom.load($(this).data('href'));
-            });
+            container.find('a[data-href]').click(onclick);
         },
         update: function(data){
             $(document).scrollTop(0);
+
             if (data.error_msg){
                 $(document).message('error', data.error_msg);
                 return;
             }
-            else if(data.content){
+
+            if ('content' in data){
                 var $content = $('.content');
                 $content.html(data.content);
                 waxe.dom.addPushStateOnLinks($content);
                 waxe.form.load();
                 waxe.versioning.init();
-                if(data.jstree_data){
-                    waxe.jstree.load(data.jstree_data);
-                }
-                else{
-                    $('body').data('layout').hide('east');
-                }
                 $(document).message('info', 'Loaded!');
             }
+
             var $breadcrumb = $('.breadcrumb');
-            if (data.breadcrumb){
+            if ('breadcrumb' in data){
                 $breadcrumb.html(data.breadcrumb);
                 waxe.dom.addPushStateOnLinks($breadcrumb);
             }
             else{
                 $breadcrumb.html('');
+            }
+
+            if('jstree_data' in data){
+                waxe.jstree.load(data.jstree_data);
+            }
+            else{
+                $('body').data('layout').hide('east');
             }
         },
         load: function(url){
---
+++ webmedia/test/dom.js
@@ -0,0 +1,184 @@
+var waxe = waxe || {};
+(function($) {
+
+    var FakeLayout = function(){
+        this.$elt = $('<div/>');
+        this.hide = function(){
+            this.$elt.hide();
+        };
+    };
+
+    var versioning_nb_called = 0;
+    var FakeVersioning = {
+        init: function(){
+            versioning_nb_called += 1;
+        }
+    };
+
+    var jstree_nb_called = 0;
+    var FakeJstree = {
+        load: function(){
+            jstree_nb_called += 1;
+        }
+    };
+
+    var message_error_nb_called = 0;
+    var message_info_nb_called = 0;
+    var message_success_nb_called = 0;
+    $.fn.message = function(type){
+        if (type === 'success'){
+            message_success_nb_called += 1;
+        }
+        else if (type === 'info'){
+            message_info_nb_called += 1;
+        }
+        else {
+            message_error_nb_called += 1;
+        }
+    };
+
+    var breadcrumb = $('<div class="breadcrumb" />');
+    var content = $('<div class="content" />');
+    var layout = new FakeLayout();
+    var old_waxe_jstree;
+    var old_waxe_versioning;
+    QUnit.module('Test waxe.dom', {
+        setup: function(){
+            old_waxe_jstree = waxe.jstree;
+            old_waxe_versioning = waxe.versioning;
+            waxe.jstree = FakeJstree;
+            waxe.versioning = FakeVersioning;
+            $('#qunit-fixture').html(breadcrumb);
+            $('#qunit-fixture').append(content);
+            $('#qunit-fixture').append(layout.$elt);
+            $('body').data('layout', layout);
+            versioning_nb_called = 0;
+            jstree_nb_called = 0;
+            message_error_nb_called = 0;
+            message_success_nb_called = 0;
+            message_info_nb_called = 0;
+        },
+        teardown: function(){
+            waxe.jstree = old_waxe_jstree;
+            waxe.versioning = old_waxe_versioning;
+        }
+    });
+
+    test("addPushStateOnLinks", function(){
+        old_waxe_dom_load = waxe.dom.load;
+        var cnt = 0;
+        waxe.dom.load = function(){
+            cnt += 1;
+        }
+        var a = $('<a data-href="/plop">plop</a>');
+        $('#qunit-fixture').append(a);
+        equal(window.history.length, 1, 'initial history');
+        waxe.dom.addPushStateOnLinks($('#qunit-fixture'));
+        a.trigger('click');
+        equal(window.history.length, 2, 'history is updated');
+        equal(cnt, 1, 'page loaded');
+        waxe.dom.load = old_waxe_dom_load;
+    });
+
+    test("update", function() {
+        var old_waxe_dom_addPushStateOnLinks = waxe.dom.addPushStateOnLinks;
+        var cnt = 0;
+        waxe.dom.addPushStateOnLinks = function(){
+            cnt += 1;
+        };
+        equal(layout.$elt.is(':visible'), true, 'layout displayed by default');
+        var data = {};
+        waxe.dom.update(data);
+        equal(content.html(), '', 'no content');
+        equal(breadcrumb.html(), '', 'no breadcrumb');
+        equal(layout.$elt.is(':visible'), false, 'layout is hidden');
+
+        data = {
+            content: 'My content',
+            breadcrumb: 'My breadcrumb'
+        };
+        waxe.dom.update(data);
+        equal(content.html(), 'My content', 'content updated');
+        equal(breadcrumb.html(), 'My breadcrumb', 'breadcrumb updated');
+        equal(layout.$elt.is(':visible'), false, 'layout is always hidden');
+        equal(versioning_nb_called, 1);
+        equal(jstree_nb_called, 0);
+        equal(message_error_nb_called,  0);
+        equal(message_success_nb_called,  0);
+        equal(message_info_nb_called, 1);
+        equal(cnt, 2);
+
+        data = {
+            content: 'My new content',
+        };
+        waxe.dom.update(data);
+        equal(content.html(), 'My new content', 'content updated');
+        equal(breadcrumb.html(), '', 'breadcrumb is empty');
+        equal(layout.$elt.is(':visible'), false, 'layout is always hidden');
+        equal(versioning_nb_called, 2);
+        equal(jstree_nb_called, 0);
+        equal(message_error_nb_called,  0);
+        equal(message_success_nb_called,  0);
+        equal(message_info_nb_called, 2);
+        equal(cnt, 3);
+
+        data = {
+            breadcrumb: 'My breadcrumb'
+        };
+        waxe.dom.update(data);
+        equal(content.html(), 'My new content', 'content not updated');
+        equal(breadcrumb.html(), 'My breadcrumb', 'breadcrumb updated');
+        equal(layout.$elt.is(':visible'), false, 'layout is always hidden');
+        equal(versioning_nb_called, 2);
+        equal(jstree_nb_called, 0);
+        equal(message_error_nb_called,  0);
+        equal(message_success_nb_called,  0);
+        equal(message_info_nb_called, 2);
+        equal(cnt, 4);
+
+        data = {
+            jstree_data: 'My data'
+        };
+        waxe.dom.update(data);
+        equal(content.html(), 'My new content', 'content not updated');
+        equal(breadcrumb.html(), '', 'breadcrumb updated');
+        equal(layout.$elt.is(':visible'), false, 'layout is always hidden');
+        equal(versioning_nb_called, 2);
+        equal(jstree_nb_called, 1);
+        equal(message_error_nb_called,  0);
+        equal(message_success_nb_called,  0);
+        equal(message_info_nb_called, 2);
+        equal(cnt, 4);
+
+        data = {
+            error_msg: 'My error'
+        };
+        // Nothing has changed, we just display the error message!
+        waxe.dom.update(data);
+        equal(content.html(), 'My new content', 'content not updated');
+        equal(breadcrumb.html(), '', 'breadcrumb updated');
+        equal(layout.$elt.is(':visible'), false, 'layout is always hidden');
+        equal(versioning_nb_called, 2);
+        equal(jstree_nb_called, 1);
+        equal(message_error_nb_called,  1);
+        equal(message_success_nb_called,  0);
+        equal(message_info_nb_called, 2);
+        equal(cnt, 4);
+
+        waxe.dom.addPushStateOnLinks = old_waxe_dom_addPushStateOnLinks;
+    });
+
+    test("load", function() {
+        var old_waxe_ajax_GET = waxe.ajax.GET;
+        waxe.ajax.GET = function(url, callback){
+            callback({'content': url + ' was loaded'});
+        };
+        waxe.dom.load('http://plop');
+        equal(content.html(), 'http://plop was loaded');
+        equal(message_error_nb_called,  0);
+        equal(message_success_nb_called,  0);
+        equal(message_info_nb_called, 2);
+        waxe.ajax.GET = old_waxe_ajax_GET;
+    });
+
+})(window.jQuery);
