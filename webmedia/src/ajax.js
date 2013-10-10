70ee54c68fc5d0d12e4020af71a2fb72121c0aa4
Aurélien Matouillot <a.matouillot@gmail.com>
2013-10-10 19:37:39 +0200

    Support async parameter in waxe.ajax

--- webmedia/src/ajax.js
+++ webmedia/src/ajax.js
@@ -4,10 +4,12 @@ var waxe = waxe || {};
     "use strict";
 
 
-    var ajax = function(type, url, callback, data){
+    var ajax = function(type, url, callback, data, async){
+        async = typeof async === 'undefined'? true: async;
         $.ajax({
             type: type,
             url: url,
+            async: async,
             data: data,
             dataType: 'json',
             success: function(data, textStatus, jqXHR){
@@ -22,11 +24,11 @@ var waxe = waxe || {};
     };
 
     ns.ajax = {
-         GET: function(url, callback){
-             ajax('GET', url, callback);
+         GET: function(url, callback, async){
+             ajax('GET', url, callback, null, async);
          },
-         POST: function(url, data, callback){
-             ajax('POST', url, callback, data);
+         POST: function(url, data, callback, async){
+             ajax('POST', url, callback, data, async);
          }
      };
 })(jQuery, waxe);
--- webmedia/test/ajax.js
+++ webmedia/test/ajax.js
@@ -16,22 +16,40 @@ var waxe = waxe || {};
     });
 
     test("GET", function() {
-        expect(4);
+        expect(10);
         waxe.ajax.GET('/test', function(params){
             ok(true, 'callback was called');
             equal(params.type, 'GET', 'GET method');
             equal(params.url, '/test', 'url');
             equal(params.dataType, 'json', 'json');
+            equal(params.async, true, 'async is true');
         });
+
+        waxe.ajax.GET('/test', function(params){
+            ok(true, 'callback was called');
+            equal(params.type, 'GET', 'GET method');
+            equal(params.url, '/test', 'url');
+            equal(params.dataType, 'json', 'json');
+            equal(params.async, false, 'async is false');
+        }, false);
     });
 
     test("POST", function() {
-        expect(4);
+        expect(10);
         waxe.ajax.POST('/test', {}, function(params){
             ok(true, 'callback was called');
             equal(params.type, 'POST', 'POST method');
             equal(params.url, '/test', 'url');
             equal(params.dataType, 'json', 'json');
+            equal(params.async, true, 'async is true');
         });
+
+        waxe.ajax.POST('/test', {}, function(params){
+            ok(true, 'callback was called');
+            equal(params.type, 'POST', 'POST method');
+            equal(params.url, '/test', 'url');
+            equal(params.dataType, 'json', 'json');
+            equal(params.async, false, 'async is false');
+        }, false);
     });
 })(jQuery);
