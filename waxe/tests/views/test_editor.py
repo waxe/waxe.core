9ccca553e22ab9aefbdd0ba6cc440fc7d6e6d341
Aurélien Matouillot <a.matouillot@gmail.com>
2013-10-26 01:04:15 +0200

    Add editor view

---
+++ waxe/tests/views/test_editor.py
@@ -0,0 +1,408 @@
+import os
+import json
+from pyramid import testing
+from mock import patch
+from urllib2 import HTTPError
+from ..testing import WaxeTestCase, login_user, LoggedBobTestCase
+
+from waxe.views.editor import (
+    EditorView,
+    _get_tags
+)
+
+
+class TestEditorView(LoggedBobTestCase):
+
+    def test__get_tags(self):
+        dtd_url = 'http://xmltool.lereskp.fr/static/exercise.dtd'
+        res = _get_tags(dtd_url)
+        expected = ['Exercise', 'comments', 'mqm', 'qcm', 'test']
+        self.assertEqual(res, expected)
+
+    def test_edit(self):
+        class C(object): pass
+        path = os.path.join(os.getcwd(), 'waxe/tests/files')
+        self.user_bob.config.root_path = path
+        request = testing.DummyRequest()
+        expected = {
+            'error_msg': 'A filename should be provided',
+        }
+        res = EditorView(request).edit()
+        self.assertEqual(res, expected)
+
+        with patch('xmltool.generate_form', return_value='My form content'):
+            expected_breadcrumb = (
+                '<li><a data-href="/filepath" href="/filepath">root</a> '
+                '<span class="divider">/</span></li>'
+                '<li class="active">file1.xml</li>')
+            request = testing.DummyRequest(
+                params={'filename': 'file1.xml'})
+            request.route_path = lambda *args, **kw: '/filepath'
+            request.matched_route = C()
+            request.matched_route.name = 'route_json'
+            res = EditorView(request).edit()
+            keys = res.keys()
+            keys.sort()
+            self.assertEqual(keys, ['breadcrumb', 'content', 'jstree_data'])
+            self.assertEqual(res['breadcrumb'],  expected_breadcrumb)
+            self.assertTrue('<form method="POST" id="xmltool-form">' in
+                            res['content'])
+            self.assertTrue(isinstance(res['jstree_data'], dict))
+
+            request.matched_route.name = 'route'
+            res = EditorView(request).edit()
+            keys = res.keys()
+            keys.sort()
+            self.assertEqual(keys, ['breadcrumb', 'content', 'jstree_data'])
+            self.assertEqual(res['breadcrumb'],  expected_breadcrumb)
+            self.assertTrue('<form method="POST" id="xmltool-form">' in
+                            res['content'])
+            self.assertTrue(isinstance(res['jstree_data'], str))
+
+        def raise_func(*args, **kw):
+            raise Exception('My error')
+
+        with patch('xmltool.load') as m:
+            m.side_effect = raise_func
+            expected = {
+                'error_msg': 'My error',
+            }
+            request = testing.DummyRequest(
+                params={'filename': 'file1.xml'})
+            request.matched_route = C()
+            request.matched_route.name = 'route_json'
+            res = EditorView(request).edit()
+            self.assertEqual(res, expected)
+
+        def raise_http_func(*args, **kw):
+            raise HTTPError('http://url', 404, 'Not found', [], None)
+
+        with patch('xmltool.load') as m:
+            m.side_effect = raise_http_func
+            expected = {
+                'error_msg': 'The dtd of file1.xml can\'t be loaded.',
+            }
+            request = testing.DummyRequest(
+                params={'filename': 'file1.xml'})
+            request.matched_route = C()
+            request.matched_route.name = 'route_json'
+            res = EditorView(request).edit()
+            self.assertEqual(res, expected)
+
+    def test_get_tags(self):
+        request = testing.DummyRequest()
+        res = EditorView(request).get_tags()
+        self.assertEqual(res, {})
+
+        dtd_url = 'http://xmltool.lereskp.fr/static/exercise.dtd'
+        request = testing.DummyRequest(params={'dtd_url': dtd_url})
+        res = EditorView(request).get_tags()
+        expected = {'tags': ['Exercise', 'comments', 'mqm', 'qcm', 'test']}
+        self.assertEqual(res, expected)
+
+    def test_new(self):
+        dtd_url = 'http://xmltool.lereskp.fr/static/exercise.dtd'
+        request = testing.DummyRequest()
+        request.dtd_urls = [dtd_url]
+        res = EditorView(request).new()
+        self.assertEqual(len(res), 1)
+        self.assertTrue('<h3>New file</h3>' in res['content'])
+
+        request = testing.DummyRequest(
+            params={
+                'dtd_url': dtd_url,
+                'dtd_tag': 'Exercise'
+            })
+        request.route_path = lambda *args, **kw: '/filepath'
+        res = EditorView(request).new()
+        self.assertEqual(len(res), 2)
+        self.assertTrue('<form method="POST" id="xmltool-form">'
+                        in res['content'])
+        self.assertTrue('<a data-href="/filepath" href="/filepath">root</a>'
+                        in res['breadcrumb'])
+
+    def test_update(self):
+        path = os.path.join(os.getcwd(), 'waxe/tests/files')
+        self.user_bob.config.root_path = path
+        request = testing.DummyRequest(params={})
+        res = EditorView(request).update()
+        expected = {'status': False, 'error_msg': 'No filename given'}
+        self.assertEqual(res, expected)
+
+        with patch('xmltool.update', return_value=False):
+            request = testing.DummyRequest(
+                params={'_xml_filename': 'test.xml'})
+            request.route_path = lambda *args, **kw: '/filepath'
+            res = EditorView(request).update()
+            expected = {
+                'status': True,
+                'breadcrumb': (
+                    '<li><a data-href="/filepath" href="/filepath">root</a> '
+                    '<span class="divider">/</span></li>'
+                    '<li class="active">test.xml</li>')
+            }
+            self.assertEqual(res, expected)
+
+        def raise_func(*args, **kw):
+            raise Exception('My error')
+
+        with patch('xmltool.update') as m:
+            m.side_effect = raise_func
+            request = testing.DummyRequest(
+                params={'_xml_filename': 'test.xml'})
+            request.route_path = lambda *args, **kw: '/filepath'
+            expected = {
+                'status': False,
+                'error_msg': 'My error',
+            }
+            res = EditorView(request).update()
+            self.assertEqual(res, expected)
+
+    def test_update_text(self):
+        path = os.path.join(os.getcwd(), 'waxe/tests/files')
+        self.user_bob.config.root_path = path
+        request = testing.DummyRequest(params={})
+        res = EditorView(request).update_text()
+        expected = {'status': False, 'error_msg': 'Missing parameters!'}
+        self.assertEqual(res, expected)
+
+        request = testing.DummyRequest(
+            params={'filecontent': 'content of the file',
+                    'filename': 'thefilename.xml'})
+
+        def raise_func(*args, **kw):
+            raise Exception('My error')
+
+        with patch('xmltool.load_string') as m:
+            m.side_effect = raise_func
+            res = EditorView(request).update_text()
+            expected = {'status': False, 'error_msg': 'My error'}
+            self.assertEqual(res,  expected)
+
+        filecontent = open(os.path.join(path, 'file1.xml'), 'r').read()
+        # The dtd should be an absolute url!
+        filecontent = filecontent.replace('exercise.dtd',
+                                          os.path.join(path, 'exercise.dtd'))
+        request = testing.DummyRequest(
+            params={'filecontent': filecontent,
+                    'filename': 'thefilename.xml'})
+
+        with patch('xmltool.elements.Element.write', return_value=None):
+            res = EditorView(request).update_text()
+            expected = {'status': True, 'content': 'File updated'}
+            self.assertEqual(res,  expected)
+
+            request.params['commit'] = True
+            res = EditorView(request).update_text()
+            self.assertEqual(len(res), 2)
+            self.assertEqual(res['status'], True)
+            self.assertTrue('class="modal' in res['content'])
+            self.assertTrue('Commit message' in res['content'])
+
+    def test_update_texts(self):
+        path = os.path.join(os.getcwd(), 'waxe/tests/files')
+        self.user_bob.config.root_path = path
+        request = testing.DummyRequest(params={})
+        res = EditorView(request).update_texts()
+        expected = {'status': False, 'error_msg': 'Missing parameters!'}
+        self.assertEqual(res, expected)
+
+        request = testing.DummyRequest(
+            params={
+                'data:0:filecontent': 'content of the file 1',
+                'data:0:filename': 'thefilename1.xml',
+                'data:1:filecontent': 'content of the file 2',
+                'data:1:filename': 'thefilename2.xml',
+            })
+
+        def raise_func(*args, **kw):
+            raise Exception('My error')
+
+        with patch('xmltool.load_string') as m:
+            m.side_effect = raise_func
+            res = EditorView(request).update_texts()
+            expected = {'status': False, 'error_msg': 'thefilename1.xml: My error<br />thefilename2.xml: My error'}
+            self.assertEqual(res,  expected)
+
+        filecontent = open(os.path.join(path, 'file1.xml'), 'r').read()
+        filecontent = filecontent.replace('exercise.dtd',
+                                          os.path.join(path, 'exercise.dtd'))
+        request = testing.DummyRequest(
+            params={'data:0:filecontent': filecontent,
+                    'data:0:filename': 'thefilename.xml'})
+
+        with patch('xmltool.elements.Element.write', return_value=None):
+            res = EditorView(request).update_texts()
+            expected = {'status': True, 'content': 'Files updated'}
+            self.assertEqual(res,  expected)
+
+            request.params['commit'] = True
+            res = EditorView(request).update_texts()
+            self.assertEqual(len(res), 2)
+            self.assertEqual(res['status'], True)
+            self.assertTrue('class="modal' in res['content'])
+            self.assertTrue('Commit message' in res['content'])
+
+    def test_add_element_json(self):
+        path = os.path.join(os.getcwd(), 'waxe/tests/files')
+        request = testing.DummyRequest(params={})
+        expected = {'status': False, 'error_msg': 'Bad parameter'}
+        res = EditorView(request).add_element_json()
+        self.assertEqual(res, expected)
+
+        dtd_url = os.path.join(path, 'exercise.dtd')
+        request = testing.DummyRequest(params={'dtd_url': dtd_url,
+                                               'elt_id': 'Exercise'})
+        res = EditorView(request).add_element_json()
+        self.assertTrue(res)
+        self.assertTrue(isinstance(res, dict))
+
+
+class FunctionalTestEditorView(WaxeTestCase):
+
+    def test_forbidden(self):
+
+        for url in [
+            '/edit.json',
+            '/get-tags.json',
+            '/new.json',
+            '/update.json',
+            '/update-text.json',
+            '/update-texts.json',
+            '/add-element.json',
+            '/get-comment-modal.json',
+        ]:
+            res = self.testapp.get(url, status=302)
+            self.assertTrue('http://localhost/login?next=' in res.location)
+            res = res.follow()
+            self.assertEqual(res.status, "200 OK")
+            self.assertTrue('<form' in res.body)
+            self.assertTrue('Login' in res.body)
+
+    @login_user('Bob')
+    def test_edit(self):
+        path = os.path.join(os.getcwd(), 'waxe/tests/files')
+        self.user_bob.config.root_path = path
+        res = self.testapp.get('/edit.json', status=200)
+        expected = '{"error_msg": "A filename should be provided"}'
+        self.assertEqual(res.body,  expected)
+        self.assertTrue(('Content-Type', 'application/json; charset=UTF-8') in
+                        res._headerlist)
+
+        res = self.testapp.get('/edit.json',
+                               status=200,
+                               params={'filename': 'file1.xml'})
+        dic = json.loads(res.body)
+        self.assertEqual(len(dic), 3)
+        self.assertTrue('<form method="POST" id="xmltool-form">' in
+                        dic['content'])
+        self.assertTrue(isinstance(dic['jstree_data'], dict))
+
+    @login_user('Bob')
+    def test_get_tags(self):
+        dtd_url = 'http://xmltool.lereskp.fr/static/exercise.dtd'
+        path = os.path.join(os.getcwd(), 'waxe/tests/files')
+        self.user_bob.config.root_path = path
+        res = self.testapp.get('/get-tags.json', status=200)
+        self.assertEqual(json.loads(res.body), {})
+
+        res = self.testapp.get('/get-tags.json',
+                               status=200,
+                               params={'dtd_url': dtd_url})
+        expected = {'tags': ['Exercise', 'comments', 'mqm', 'qcm', 'test']}
+        self.assertEqual(json.loads(res.body), expected)
+
+    @login_user('Bob')
+    def test_new(self):
+        path = os.path.join(os.getcwd(), 'waxe/tests/files')
+        self.user_bob.config.root_path = path
+        res = self.testapp.get('/new.json', status=200)
+        self.assertTrue(('Content-Type', 'application/json; charset=UTF-8') in
+                        res._headerlist)
+        dic = json.loads(res.body)
+        self.assertEqual(len(dic), 1)
+        self.assertTrue('<h3>New file</h3>' in dic['content'])
+
+        dtd_url = 'http://xmltool.lereskp.fr/static/exercise.dtd'
+        dtd_tag = 'Exercise'
+        res = self.testapp.get('/new.json',
+                               status=200,
+                               params={'dtd_url': dtd_url,
+                                       'dtd_tag': dtd_tag})
+        dic = json.loads(res.body)
+        self.assertEqual(len(dic), 2)
+        self.assertTrue('<form method="POST" id="xmltool-form">' in
+                        dic['content'])
+        self.assertTrue(dic['breadcrumb'])
+        self.assertTrue('data-href="/home.json?path="' in dic['breadcrumb'])
+
+    @login_user('Bob')
+    def test_update(self):
+        path = os.path.join(os.getcwd(), 'waxe/tests/files')
+        self.user_bob.config.root_path = path
+        res = self.testapp.post('/update.json', status=200)
+        self.assertTrue(('Content-Type', 'application/json; charset=UTF-8') in
+                        res._headerlist)
+        expected = {"status": False, "error_msg": "No filename given"}
+        self.assertEqual(json.loads(res.body), expected)
+
+        with patch('xmltool.update', return_value=False):
+            res = self.testapp.post('/update.json',
+                                    status=200,
+                                    params={'_xml_filename': 'test.xml'})
+            expected = {
+                "status": True,
+                "breadcrumb": (
+                    "<li><a data-href=\"/home.json?path=\" href=\"/?path=\">root</a> "
+                    "<span class=\"divider\">/</span></li>"
+                    "<li class=\"active\">test.xml</li>")}
+        self.assertEqual(json.loads(res.body), expected)
+
+    @login_user('Bob')
+    def test_update_text(self):
+        path = os.path.join(os.getcwd(), 'waxe/tests/files')
+        self.user_bob.config.root_path = path
+        res = self.testapp.post('/update-text.json', status=200)
+        self.assertTrue(('Content-Type', 'application/json; charset=UTF-8') in
+                        res._headerlist)
+        expected = {"status": False, "error_msg": "Missing parameters!"}
+        self.assertEqual(json.loads(res.body), expected)
+
+    @login_user('Bob')
+    def test_update_texts(self):
+        path = os.path.join(os.getcwd(), 'waxe/tests/files')
+        self.user_bob.config.root_path = path
+        res = self.testapp.post('/update-texts.json', status=200)
+        self.assertTrue(('Content-Type', 'application/json; charset=UTF-8') in
+                        res._headerlist)
+        expected = {"status": False, "error_msg": "Missing parameters!"}
+        self.assertEqual(json.loads(res.body), expected)
+
+    @login_user('Bob')
+    def test_add_element_json(self):
+        path = os.path.join(os.getcwd(), 'waxe/tests/files')
+        self.user_bob.config.root_path = path
+        res = self.testapp.get('/add-element.json', status=200)
+        self.assertTrue(('Content-Type', 'application/json; charset=UTF-8') in
+                        res._headerlist)
+        expected = {"status": False, "error_msg": "Bad parameter"}
+        self.assertEqual(json.loads(res.body), expected)
+
+        dtd_url = os.path.join(path, 'exercise.dtd')
+        res = self.testapp.get('/add-element.json', status=200,
+                               params={'dtd_url': dtd_url,
+                                       'elt_id': 'Exercise'})
+
+        dic = json.loads(res.body)
+        self.assertTrue(dic['status'])
+
+    @login_user('Bob')
+    def test_get_comment_modal_json(self):
+        path = os.path.join(os.getcwd(), 'waxe/tests/files')
+        self.user_bob.config.root_path = path
+        res = self.testapp.get('/get-comment-modal.json', status=200)
+        self.assertTrue(('Content-Type', 'application/json; charset=UTF-8') in
+                        res._headerlist)
+        body = json.loads(res.body)
+        self.assertEqual(len(body), 1)
+        self.assertTrue('<div class="modal ' in body['content'])
---
+++ waxe/views/editor.py
@@ -0,0 +1,189 @@
+import logging
+import xmltool
+import json
+from urllib2 import HTTPError
+from pyramid.view import view_config
+from pyramid.renderers import render
+from .. import browser
+from ..utils import unflatten_params
+from base import BaseUserView
+
+log = logging.getLogger(__name__)
+
+
+def _get_tags(dtd_url):
+    dic = xmltool.dtd_parser.parse(dtd_url=dtd_url)
+    lis = []
+    for k, v in dic.items():
+        if issubclass(v, xmltool.elements.TextElement):
+            continue
+        lis += [k]
+    lis.sort()
+    return lis
+
+
+class EditorView(BaseUserView):
+
+    @view_config(route_name='edit', renderer='index.mak', permission='edit')
+    @view_config(route_name='edit_json', renderer='json', permission='edit')
+    def edit(self):
+        filename = self.request.GET.get('filename') or ''
+        if not filename:
+            return {
+                'error_msg': 'A filename should be provided',
+            }
+        root_path = self.root_path
+        absfilename = browser.absolute_path(filename, root_path)
+        try:
+            obj = xmltool.load(absfilename)
+            html = xmltool.generate_form_from_obj(obj, form_filename=filename)
+            jstree_data = obj.to_jstree_dict([])
+            if not self._is_json():
+                jstree_data = json.dumps(jstree_data)
+        except HTTPError, e:
+            log.exception(e)
+            return {
+                'error_msg': 'The dtd of %s can\'t be loaded.' % filename
+            }
+        except Exception, e:
+            log.exception(e)
+            return {
+                'error_msg': str(e)
+            }
+        breadcrumb = self._get_breadcrumb(filename)
+        return {
+            'content': html,
+            'breadcrumb': breadcrumb,
+            'jstree_data': jstree_data,
+        }
+
+    @view_config(route_name='get_tags_json', renderer='json', permission='edit')
+    def get_tags(self):
+        dtd_url = self.request.GET.get('dtd_url', None)
+
+        if not dtd_url:
+            return {}
+
+        return {'tags': _get_tags(dtd_url)}
+
+    @view_config(route_name='new_json', renderer='json', permission='edit')
+    def new(self):
+        dtd_url = self.request.GET.get('dtd_url') or None
+        dtd_tag = self.request.GET.get('dtd_tag') or None
+
+        if dtd_tag and dtd_url:
+            html = xmltool.new(dtd_url, dtd_tag)
+            return {
+                'content': html,
+                'breadcrumb': self._get_breadcrumb(None, force_link=True),
+            }
+
+        content = render('blocks/new.mak',
+                         {'dtd_urls': self.request.dtd_urls,
+                          'tags': _get_tags(self.request.dtd_urls[0])},
+                         self.request)
+        return {'content': content}
+
+    @view_config(route_name='update_json', renderer='json', permission='edit')
+    def update(self):
+        filename = self.request.POST.pop('_xml_filename', None)
+        if not filename:
+            return {'status': False, 'error_msg': 'No filename given'}
+
+        root_path = self.root_path
+        absfilename = browser.absolute_path(filename, root_path)
+        try:
+            xmltool.update(absfilename, self.request.POST)
+        except Exception, e:
+            log.exception(e)
+            return {'status': False, 'error_msg': str(e)}
+
+        return {
+            'status': True,
+            'breadcrumb': self._get_breadcrumb(filename)
+        }
+
+    @view_config(route_name='update_text_json', renderer='json', permission='edit')
+    def update_text(self):
+        filecontent = self.request.POST.get('filecontent')
+        filename = self.request.POST.get('filename') or ''
+        if not filecontent or not filename:
+            return {'status': False, 'error_msg': 'Missing parameters!'}
+        root_path = self.root_path
+        absfilename = browser.absolute_path(filename, root_path)
+        try:
+            obj = xmltool.load_string(filecontent)
+            obj.write(absfilename)
+        except Exception, e:
+            return {'status': False, 'error_msg': str(e)}
+
+        content = 'File updated'
+        if self.request.POST.get('commit'):
+            content = render('blocks/commit_modal.mak',
+                             {}, self.request)
+
+        return {'status': True, 'content': content}
+
+    @view_config(route_name='update_texts_json', renderer='json', permission='edit')
+    def update_texts(self):
+        params = unflatten_params(self.request.POST)
+
+        if 'data' not in params or not params['data']:
+            return {'status': False, 'error_msg': 'Missing parameters!'}
+
+        root_path = self.root_path
+        status = True
+        error_msgs = []
+        for dic in params['data']:
+            filecontent = dic['filecontent']
+            filename = dic['filename']
+            absfilename = browser.absolute_path(filename, root_path)
+            try:
+                obj = xmltool.load_string(filecontent)
+                obj.write(absfilename)
+            except Exception, e:
+                status = False
+                error_msgs += ['%s: %s' % (filename, str(e))]
+
+        if not status:
+            return {'status': False, 'error_msg': '<br />'.join(error_msgs)}
+
+        content = 'Files updated'
+        if self.request.POST.get('commit'):
+            content = render('blocks/commit_modal.mak',
+                             {}, self.request)
+
+        return {'status': True, 'content': content}
+
+    @view_config(route_name='add_element_json', renderer='json',
+                 permission='edit')
+    def add_element_json(self):
+        elt_id = self.request.GET.get('elt_id')
+        dtd_url = self.request.GET.get('dtd_url')
+        if not elt_id or not dtd_url:
+            return {'status': False, 'error_msg': 'Bad parameter'}
+        dic = xmltool.elements.get_jstree_json_from_str_id(elt_id,
+                                                           dtd_url=dtd_url)
+        dic['status'] = True
+        return dic
+
+    @view_config(route_name='get_comment_modal_json', renderer='json',
+                 permission='edit')
+    def get_comment_modal_json(self):
+        comment = self.request.GET.get('comment') or ''
+        content = render('blocks/comment_modal.mak',
+                         {'comment': comment}, self.request)
+        return {'content': content}
+
+
+def includeme(config):
+    config.add_route('edit', '/edit')
+    config.add_route('edit_json', '/edit.json')
+    config.add_route('get_tags_json', '/get-tags.json')
+    config.add_route('new_json', '/new.json')
+    config.add_route('update_json', '/update.json')
+    config.add_route('update_text_json', '/update-text.json')
+    config.add_route('update_texts_json', '/update-texts.json')
+    config.add_route('add_element_json', '/add-element.json')
+    config.add_route('get_comment_modal_json', '/get-comment-modal.json')
+    config.scan(__name__)
