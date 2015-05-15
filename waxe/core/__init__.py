76f5ae45b598ccafc45f7f0760f5c7e6efe9833b
Aurélien Matouillot <a.matouillot@gmail.com>
2015-05-15 18:31:21 +0200

    No xml logic in the core

--- waxe/core/__init__.py
+++ waxe/core/__init__.py
@@ -1,6 +1,5 @@
 import os
 import locale
-import importlib
 
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
@@ -39,24 +38,6 @@ def get_views_modules(settings, waxe_editors, waxe_renderers):
     return lis
 
 
-def get_dtd_urls(request):
-    if 'dtd_urls' not in request.registry.settings:
-        raise AttributeError('No dtd_urls defined in the ini file.')
-    return filter(bool, request.registry.settings['dtd_urls'].split('\n'))
-
-
-def get_xml_plugins(request):
-    if 'waxe.xml.plugins' not in request.registry.settings:
-        return []
-    lis = filter(bool,
-                 request.registry.settings['waxe.xml.plugins'].split('\n'))
-
-    mods = []
-    for s in lis:
-        mods.append(importlib.import_module(s))
-    return mods
-
-
 def get_str_resources(request):
     return resource.STR_RESOURCES
 
@@ -86,10 +67,6 @@ def main(global_config, **settings):
     encoding = encoding or 'UTF8'
     locale.setlocale(locale.LC_ALL, '%s.%s' % (language_code, encoding))
 
-    cache_timeout = settings.get('xmltool.cache_timeout')
-    if cache_timeout:
-        os.environ['XMLTOOL_CACHE_TIMEOUT'] = cache_timeout
-
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
@@ -100,28 +77,27 @@ def main(global_config, **settings):
     # Get the editors and the extensions supported
     waxe_editors = config_parser.parse_waxe_editors(settings)
     waxe_renderers = config_parser.parse_waxe_renderers(settings)
+
     # TODO: extensions should be split in both: editor and renderer
     extensions = sum([exts for exts, mod in waxe_editors], [])
     settings['waxe.extensions'] = extensions
 
-    session_key = settings['session.key']
-    session_factory = UnencryptedCookieSessionFactoryConfig(session_key)
+    session_factory = UnencryptedCookieSessionFactoryConfig(
+        settings['session.key'])
     config = Configurator(settings=settings,
                           session_factory=session_factory,
                           root_factory=RootFactory)
-    config.add_static_view('static-core', 'waxe.core:static', cache_max_age=3600)
-
-    # TODO: not sure we need to define dtd_urls here.
-    config.set_request_property(get_dtd_urls, 'dtd_urls', reify=True)
-    config.set_request_property(get_xml_plugins, 'xml_plugins', reify=True)
+    config.add_static_view('static-core', 'waxe.core:static',
+                           cache_max_age=3600)
 
     config.set_request_property(get_str_resources, 'str_resources', reify=True)
     config.set_request_property(get_js_resources, 'js_resources', reify=True)
     config.set_request_property(get_css_resources, 'css_resources', reify=True)
 
-    for module, prefix, api, extra_prefix in get_views_modules(settings,
-                                                          waxe_editors,
-                                                          waxe_renderers):
+    for (module, prefix, api, extra_prefix) in get_views_modules(
+            settings,
+            waxe_editors,
+            waxe_renderers):
         route_prefix = None
         if api:
             route_prefix = '/api/1'
